798cd7d493ace5f7767a7f09382780647bee2aec
h-nriquevieira <h.nriquevieira@gmail.com>
2022-09-24 13:29:18 -0300

    Adicionar card de restaurante

--- src/pages/Restaurantes/RestaurantesPage.jsx
+++ src/pages/Restaurantes/RestaurantesPage.jsx
@@ -1,5 +1,7 @@
 import { Container, Typography, CircularProgress } from "@material-ui/core";
 import { useEffect, useState } from "react";
+import { useParams } from 'react-router-dom'
+import RestauranteCard from "../../components/RestauranteCard/RestauranteCard";
 import { getRestaurantes } from "../../services/restaurantes.service";
 import "./style.css";
 
@@ -9,9 +11,10 @@ function RestaurantesPage() {
   const [restaurantesNoPreco, setRestaurantesNoPreco] = useState([]);
   const [restaurantesCaro, setRestaurantesCaro] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { id } = useParams()
 
   useEffect(() => {
-    getRestaurantes().then((response) => {
+    getRestaurantes(id).then((response) => {
       setNomeCategoria(response.categoria)
       setRestaurantesBaratinho(response.baratinho);
       setRestaurantesNoPreco(response.no_preco);
@@ -37,11 +40,53 @@ function RestaurantesPage() {
       </div>
       {restaurantesBaratinho?.map(restaurante => (
         <div key={restaurante.id}>
+          <RestauranteCard restaurante={restaurante} />
+        </div>
+      ))}
+      <div className="sub-header">
+        <Typography variant="body1" color="primary">
+          No preço <span>(</span>$ $ $<span> $ $)</span>
+        </Typography>
+      </div>
+      {restaurantesNoPreco?.map(restaurante => (
+        <div key={restaurante.id}>
+          {restaurante.nome}
+        </div>
+      ))}
+      <div className="sub-header">
+        <Typography variant="body1" color="primary">
+          Caro, mas vale a pena <span>(</span>$ $ $ $ $<span>)</span>
+        </Typography>
+      </div>
+      {restaurantesCaro?.map(restaurante => (
+        <div key={restaurante.id}>
           {restaurante.nome}
         </div>
       ))}
     </Container>
+
+
   )
 }
 
 export default RestaurantesPage;
+// {/* <Container class="restaurantes">
+//   <Typography variant="h5" align="center" color="primary" className="title">
+//     RESTAURANTES: {nomeCategoria}
+//   </Typography>
+//   {loading && (
+//     <div className="loading">
+//       <CircularProgress color="primary" />
+//     </div>
+//   )}
+//   <div className="sub-header">
+//     <Typography variant="body1" color="primary">
+//       Baratinho <span>(</span>$ <span>$ $ $ $)</span>
+//     </Typography>
+//   </div>
+//   {restaurantesBaratinho?.map(restaurante => (
+//     <div key={restaurante.id}>
+//       {restaurante.nome}
+//     </div>
+//   ))}
+// </Container> */}
